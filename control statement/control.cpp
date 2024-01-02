260e750db7fa3d12bd601e82b9d9c3d382f60254
Amar dev panwar <127836087+AMARDEV07@users.noreply.github.com>
2024-01-03 01:13:30 +0530

    Add files via upload

---
+++ control statement/control.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+//     int age;
+//     cout<<"tell me ur age"<<endl;
+//     cin>>age;
+//     if(age<15){
+//         cout<<"you can not come my party";
+//     }
+//     else{
+
+//         cout<<"u can come";
+//     }
+//     return 0;
+// }
+
+
+
+
+
+// int a,b;
+// cout<<"insert integer"<<endl;
+// cin>>a>>b;
+
+
+// if(a==b){
+//     cout<<"they are equal";
+
+// }
+// else{
+//     cout<<"they are not";
+// }
+// return 0;
+// }
+
+
+
+int  n;
+
+cout<<"enter value of n";
+cin>>n;
+
+
+
+if(n<5){
+    cout<<"less then 5";
+
+}
+else if(n>5){
+    cout<<"greater then 5";
+}
+else{
+    cout<<"equal";
+}
+return 0;
+}
---
+++ control statement/switch cae/switch.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int age;
+    cout<<"enter age";
+    cin>>age;
+
+    switch (age)
+    {
+    case 18:
+        cout<<"age is 18";
+        break;
+    
+    default:
+        break;
+    }
+}
