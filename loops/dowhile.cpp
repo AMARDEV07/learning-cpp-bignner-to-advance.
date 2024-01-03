3097b0057040247462c6f5fbabca391bbd83853f
Amar dev panwar <127836087+AMARDEV07@users.noreply.github.com>
2024-01-04 02:38:10 +0530

    Add files via upload

---
+++ loops/dowhile.cpp
@@ -0,0 +1,20 @@
+// synatax
+// do
+// {
+//     code 
+// } while (/* condition */);
+
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i=1;
+    do
+    {
+        cout<<i<<endl;
+        i++;
+    } while (i<=4);
+    
+}
---
+++ loops/ex1.cpp
@@ -0,0 +1,46 @@
+// write 6 table using loops
+
+
+#include<iostream>
+ using namespace std;
+
+
+ int main(){
+   
+
+//    for loops;
+
+    // cout<<"multiplication of 6:"<<endl;
+    
+
+    // for(int i=1;i<=10;i++){
+
+    //     cout<<i*6<<endl;
+        
+
+    // }
+    // return 0;
+
+
+// while loops
+
+// int i=1;
+// int result=6;
+
+//     while(i<=10){
+//         cout<<i*result<<endl;
+//         i++;
+
+//     }
+//     return 0;
+    
+//  
+
+// do while
+
+int i=1;
+do{
+    cout<<i*6<<endl;
+    i++;
+}while(i<=10);
+ }
