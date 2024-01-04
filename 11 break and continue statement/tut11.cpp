e5627e31522d3fe2953e18066714696c299e5749
Amar dev panwar <127836087+AMARDEV07@users.noreply.github.com>
2024-01-05 01:22:11 +0530

    Add files via upload

---
+++ 11 break and continue statement/tut11.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+
+// int main(){
+//     int i;
+//     for(int i=1;i<10;i++){
+//         if(i==4){
+//             break;
+//         }
+//             cout<<i;
+        
+//     }
+//     return 0;
+// }
+
+
+
+
+// 
+
+
+
+int main(){
+    int i=1;
+    do
+    {
+    cout<<i;
+    i++;
+    if(i==4){
+        break;
+    }
+    } while (i<=10);
+    
+}
---
+++ 11 break and continue statement/tut11continue.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+
+// int main(){
+//     for(int i=1;i<10;i++){
+//         if(i==2){
+//             continue;
+//         }
+//         cout<<i;
+//     }
+//     return 0;
+// }
+
+
+int main(){
+    int i=1;
+
+    while(i<=10){
+        if(i==4){
+        
+            continue;
+               
+            
+        }
+    cout<<i;
+    i++;
+   
+    
+    }
+    return 0;
+}
