1967a6e4002efaa3c16ef4b24d1ef58df29b3788
Amar dev panwar <127836087+AMARDEV07@users.noreply.github.com>
2024-01-06 01:03:09 +0530

    Add files via upload

---
+++ pointer/poi.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+// int main(){
+
+//     int a=3;
+
+//     int *p=&a;
+//     cout<<"the address "<<&a<<endl;
+//     cout<<"the address"<<&p;
+//     cout<<"value"<<*p<<endl;
+
+
+//     return 0;
+
+// }
+int main(){
+    int arr[6]={1,2,3,4,5,6};
+    int *p= &arr[0];
+    cout<<arr[3];
+}
